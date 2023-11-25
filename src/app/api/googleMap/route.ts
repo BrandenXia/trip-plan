5c17a873aec3d405e5bdd9bbaf5d0fcd35291d62
BrandenXia <brandenxia@163.com>
2023-11-25 01:20:38 -0800

    Fix distance and duration format

--- src/app/api/googleMap/route.ts
+++ src/app/api/googleMap/route.ts
@@ -1,6 +1,19 @@
 import {distanceMatrix} from "@/src/utils/GoogleMapsUtils";
 import {TrafficModel} from "@googlemaps/google-maps-services-js";
 
+function formatDuration(seconds: number) {
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  return `${hours.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}`;
+}
+
+function formatDistance(meters: number) {
+  const miles = meters / 1609.344;
+  return `${miles.toFixed(1)}`;
+}
+
+
 export async function POST(request: Request) {
   const data = await request.json();
 
@@ -11,9 +24,9 @@ export async function POST(request: Request) {
     const result1 = await distanceMatrix(origin, destination, time, TrafficModel.pessimistic);
     const result2 = await distanceMatrix(origin, destination, time, TrafficModel.optimistic);
     res.push({
-      distance: result1.rows[0].elements[0].distance.text,
-      duration: [result2.rows[0].elements[0].duration_in_traffic?.text ?? result2.rows[0].elements[0].duration.text,
-        result1.rows[0].elements[0].duration_in_traffic?.text ?? result1.rows[0].elements[0].duration.text
+      distance: formatDistance(result1.rows[0].elements[0].distance.value),
+      duration: [formatDuration(result2.rows[0].elements[0].duration_in_traffic?.value ?? result2.rows[0].elements[0].duration.value),
+        formatDuration(result1.rows[0].elements[0].duration_in_traffic?.value ?? result1.rows[0].elements[0].duration.value)
       ]
     });
   }
